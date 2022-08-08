e63c10672bc253e49e9db6694b865e3edeb597a8
GN0MI0 <103611243+SauloJose@users.noreply.github.com>
2022-08-08 14:25:04 -0300

    Add files via upload

---
+++ MFP.m
@@ -0,0 +1,48 @@
+%Método da falsa posição
+function [yy,ea,X] = MFP(x0,x1,itmax,e)
+  f=@(x) x^2-7*x+12;
+  xl=x0;
+  xu=x1;
+  fu=f(xu);
+  fl=f(xl);
+  areal=-0.41468941;
+  xr=xu-fu*(xl-xu)/(fl-fu);
+  fr=f(xr);
+  err=e;
+
+  ea=abs(x1-x0)/abs(x1);
+  emaj=abs((x1-x0)/2^0);
+  erel=abs((xr-areal)/xr);
+  it=1;
+
+  do
+    it=it+1;
+    xrold=xr;
+    teste=fr*fl;
+
+    if teste<0
+      xu=xr;
+      fu=f(xu);
+    elseif teste>0
+      xl=xr;
+      fl=f(xl);
+    else
+      yy=xr;
+      break;
+    endif
+    xr=xu-fu*(xl-xu)/(fl-fu);
+    fr=f(xr);
+
+    %calculando erros
+    ea(it)=abs((xr-xrold)/xr);
+    erel(it)=abs((xr-areal)/xr);
+    emaj(it)=abs((x1-x0)/2^it);
+  until ea(it) < err || it>=itmax
+
+  X=1:1:it;
+  plot(X,ea," - o ");
+  title('Gráfico de erros em função das iterações');
+  xlabel('Iterações');
+  ylabel('Erro relativo');
+  grid();
+  yy=xr;
---
+++ MNR.m
@@ -0,0 +1,25 @@
+%MÉTODO DE NEWTON-RAPSON
+
+function [yy,ea,Xa]=MNR(x0,itmax,e)
+  f=@(x) x^2-7*x+12;
+  df=@(x) 2*x-7;
+
+  err=e;
+  x(1) = x0;
+  it=1
+  do
+    it=it+1;
+    x(it)=x(it-1)-f(x(it-1))/df(x(it-1));
+    ea(it-1)=abs((x(it)-x(it-1))/x(it));
+  until abs(f(x(it))) < e || it>=itmax
+  Xa=1:1:it-1;
+  plot(Xa,ea,"-o");
+  title('Gráfico dos erros em função das iterações');
+  xlabel('iteração');
+  ylabel('Erro relativo');
+  grid();
+  yy=x(it);
+
+
+
+
